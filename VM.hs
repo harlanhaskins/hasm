343179475d54cc60d29de022934b467a5daf801d
Harlan Haskins <harlan@harlanhaskins.com>
2015-08-25 12:59:47 -0400

    Added branch and jump instructions

--- VM.hs
+++ VM.hs
@@ -2,12 +2,6 @@ module VM where
 
 import Data.Bits as B
 
-data Instruction = Mov Int Int
-                 | Load Int Int
-                 | Add Int Int Int
-                 | Sub Int Int Int
-                 | Mul Int Int Int
-
 data Register a = Register { value :: a } deriving (Show, Eq)
 
 instance Functor Register where
@@ -21,19 +15,19 @@ instance Monad Register where
     return = pure
     Register v >>= f = f v
 
-data CPU a = CPU { counter :: Int, registers :: [Register a], memory :: [Register a]} deriving (Show, Eq)
+data CPU a = CPU { registers :: [Register a], memory :: [Register a]} deriving (Show, Eq)
 
 fromValue i rs = (value . (!! i)) rs
 
-fromLists rs mems = CPU 0 (toRegisters rs) (toRegisters mems)
+fromLists rs mems = CPU (toRegisters rs) (toRegisters mems)
     where toRegisters = (map Register) 
 
-combine f r1 r2 dst (CPU c rs mem) = CPU (c+1) (replace dst final rs) mem
+combine f r1 r2 dst (CPU rs mem) = CPU (replace dst final rs) mem
     where final = pure $ f (fromValue r1 rs) (fromValue r2 rs)
 
 modify f src dst cpu = combine (\x _ -> f x) src dst dst cpu
 
-load addr dst (CPU c rs mem) = CPU (c+1) (replace dst (mem !! addr) rs) mem
+load addr dst (CPU rs mem) = CPU (replace dst (mem !! addr) rs) mem
 
 mov = modify id
 
@@ -58,16 +52,37 @@ and = combine (.&.)
 or :: (Bits a) => Int -> Int -> Int -> CPU a -> CPU a
 or = combine (.|.)
 
-run :: (Num a) => [Instruction] -> CPU a -> CPU a
-run [] cpu = cpu
-run (x:xs) cpu = run xs (runInstruction x cpu)
+data Instruction = Mov Int Int
+                 | Load Int Int
+                 | Add Int Int Int
+                 | Sub Int Int Int
+                 | Mul Int Int Int
+                 | Jmp Int
+                 | Bne Int Int Int
+                 | Beq Int Int Int
+                 deriving (Show, Eq)
 
-runInstruction :: (Num a) => Instruction -> CPU a -> CPU a
-runInstruction (Mov src dst) cpu = mov src dst cpu
-runInstruction (Load src dst) cpu = load src dst cpu
-runInstruction (Add r1 r2 dst) cpu = add r1 r2 dst cpu
-runInstruction (Sub r1 r2 dst) cpu = sub r1 r2 dst cpu
-runInstruction (Mul r1 r2 dst) cpu = mul r1 r2 dst cpu
+data Program a = Program { remaining :: [Instruction]
+                         , instructions :: [Instruction]
+                         } deriving (Show, Eq)
+
+fromInstructions xs = Program xs xs
+
+run :: (Num a, Eq a) => Program a -> CPU a -> CPU a
+run (Program [] _) cpu             = cpu
+run (Program ((Jmp idx):_) is) cpu = run (Program (drop idx is) is) cpu
+run p@(Program ((Bne r1 r2 idx):_) _) cpu = branchIf (/=) p r1 r2 idx cpu
+run p@(Program ((Beq r1 r2 idx):_) _) cpu = branchIf (==) p r1 r2 idx cpu
+run (Program (i:is) is') cpu       = run (Program is is') (runInstruction i cpu)
+    where runInstruction (Mov src dst)   cpu = mov src dst cpu
+          runInstruction (Load src dst)  cpu = load src dst cpu
+          runInstruction (Add r1 r2 dst) cpu = add r1 r2 dst cpu
+          runInstruction (Sub r1 r2 dst) cpu = sub r1 r2 dst cpu
+          runInstruction (Mul r1 r2 dst) cpu = mul r1 r2 dst cpu
+
+branchIf f (Program (i:is) is') r1 r2 idx cpu@(CPU rs mem)
+    | f (fromValue r1 rs) (fromValue r2 rs) = run (Program ((Jmp idx):is) is') cpu
+    | otherwise                             = run (Program is is') cpu
 
 replace _ item [_] = [item]
 replace n item ls = a ++ (item:b)
