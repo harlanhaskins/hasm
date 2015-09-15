314ee2dde14707953f8723648358c7cd5f2cbece
Harlan Haskins <harlan@harlanhaskins.com>
2015-09-14 21:42:51 -0400

    Added debug flag

--- src/Main.hs
+++ src/Main.hs
@@ -9,17 +9,30 @@ import qualified Data.Vector as V
 import Hasm.Core
 import Hasm.Parser
 
+data Config = Config
+    { filename     :: String
+    , debug        :: Bool
+    , values       :: [Integer]
+    }
+
 initialize (CPU c rs mem) xs = (CPU c (rs V.// (zip [0..] xs)) mem)
 
+parseArguments :: [String] -> Config
+parseArguments ("-d":fn:xs) = Config fn True  (map read xs)
+parseArguments (     fn:xs) = Config fn False (map read xs)
+
 main = do
-    (filename:xs) <- getArgs
-    file <- B.readFile filename
+    args <- getArgs
+    let config = parseArguments args
+    file <- (B.readFile . filename) config
     case parseOnly parseFile file of
         Left err -> putStrLn $ "Error parsing: " ++ err
         Right (cpu, instructions) -> do
-            let initialized = initialize cpu (map read xs)
-            putStrLn $ "Current CPU: \n" ++ (show initialized)
-            putStrLn . unlines . (map show) $ instructions
-            let final = run (V.fromList instructions) initialized
-            putStrLn $ "Final CPU: \n" ++ (show final)
-            -- runPrint (V.fromList instructions) initialized
+            let initialized = initialize cpu (values config)
+            putStrLn $ "Starting CPU: \n" ++ (show initialized)
+            if (debug config) then do
+                putStrLn . unlines . (map show) $ instructions
+                runPrint (V.fromList instructions) initialized
+            else do
+                let final = run (V.fromList instructions) initialized
+                putStrLn $ "Final CPU: \n" ++ (show final)
